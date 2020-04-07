2af3fbd38ac3968ec1b2f075772507c96bb70eda
Kochnov Andrey <a.kochnov@corp.mail.ru>
2020-04-08 02:03:44 +0300

    Добавили служебные файлы репл

---
+++ run.sh
@@ -0,0 +1,8 @@
+git add . && git commit -m "Fuck"
+git pull origin master
+echo "\e[36mBless the Butseff!!\e[0m"
+python3 main.py > log
+cat log
+echo "\e[31mFUCK ALL IT\e[0m"
+# git add .
+# git commit -m "fuck"
